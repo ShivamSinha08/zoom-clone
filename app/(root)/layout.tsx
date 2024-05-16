90417a83fbb5e85113c5728f3c6816eb8928b108
Shivam Sinha <shivamuv2003@gmail.com>
2024-05-16 17:03:47 +0530

    Sidebar Done

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const RootLayout = ({children}: {children: ReactNode}) => {
+  return (
+    <main>
+        
+      {children}
+        
+    </main>
+  )
+}
+
+export default RootLayout
---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const sidebarLinks = [
+    {
+        lable: 'Home',
+        route: '/',
+        imageUrl: 'icons/Home.svg',
+    },
+    {
+        lable: 'Upcoming',
+        route: '/upcoming',
+        imageUrl: 'icons/upcoming.svg',
+    },
+    {
+        lable: 'Previous',
+        route: '/previous',
+        imageUrl: 'icons/previous.svg',
+    },
+    {
+        lable: 'Recordings',
+        route: '/recordings',
+        imageUrl: 'icons/Video.svg',
+    },
+    {
+        lable: 'Personal Room',
+        route: '/personal-room',
+        imageUrl: 'icons/add-personal.svg',
+    }
+];
